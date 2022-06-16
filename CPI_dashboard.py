a4bae4e3a52f9071f1442170a4b0c227ab9898c8
gwk01 <78788407+gwk01@users.noreply.github.com>
2022-06-16 12:17:17 +0300

    Update CPI_dashboard.py

--- CPI_dashboard.py
+++ CPI_dashboard.py
@@ -21,7 +21,7 @@ for i in range(len(portal)):
     portal['Country'][i]=portal['Country'][i].rstrip()
 #Data date
 mydate = datetime.datetime.now()
-cpi_photo=Image.open('Capture.png')
+cpi_photo=Image.open('Capture.PNG')
 #Country
 
 country = 'Bahrain'
